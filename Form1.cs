9b75fce36efca55e39718566832785722a2dc4ec
Mdsr2op <mdsrmfkhan@gmail.com>
2025-03-25 06:35:53 +0500

    Added Boilerplate code

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _36515_SDT_Mid
+{
+    public partial class frmGroupManagement : Form
+    {
+        public frmGroupManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
